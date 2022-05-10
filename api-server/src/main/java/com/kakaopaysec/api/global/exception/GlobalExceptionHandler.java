8089225a7e812f176b85e3634060992b895c41d5
krung2 <jungbin4337@dgsw.hs.kr>
2022-05-10 17:53:07 +0900

    Feat: restControllers & handler

---
+++ api-server/src/main/java/com/kakaopaysec/api/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.kakaopaysec.api.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+
+}
