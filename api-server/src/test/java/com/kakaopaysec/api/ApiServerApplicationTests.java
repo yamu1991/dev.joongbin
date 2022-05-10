4922174a94333e08046c3cec9d4fe25b64ad90be
krung2 <jungbin4337@dgsw.hs.kr>
2022-05-10 17:45:16 +0900

    Initialize spring

---
+++ api-server/src/test/java/com/kakaopaysec/api/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaopaysec.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
