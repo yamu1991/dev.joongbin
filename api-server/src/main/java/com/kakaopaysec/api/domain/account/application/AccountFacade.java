65db57232ac80b562a53ff3ffcf8b54fb2169efb
krung2 <jungbin4337@dgsw.hs.kr>
2022-05-11 19:43:43 +0900

    Feat: getAccountList facade

--- api-server/src/main/java/com/kakaopaysec/api/domain/account/application/AccountFacade.java
+++ api-server/src/main/java/com/kakaopaysec/api/domain/account/application/AccountFacade.java
@@ -1,10 +1,19 @@
 package com.kakaopaysec.api.domain.account.application;
 
+import com.kakaopaysec.api.domain.account.domain.AccountInfo;
+import com.kakaopaysec.api.domain.account.domain.AccountServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AccountFacade {
 
+    private final AccountServiceImpl accountService;
+
+    public List<AccountInfo.Main> getAccountList() {
+        return accountService.getAccountList();
+    }
 }
