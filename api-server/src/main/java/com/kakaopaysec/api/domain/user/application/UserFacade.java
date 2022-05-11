a5ca340684f43cc740f69adf6a48c95b8ca9efcc
krung2 <jungbin4337@dgsw.hs.kr>
2022-05-11 16:13:15 +0900

    Feat: registerUser facade

--- api-server/src/main/java/com/kakaopaysec/api/domain/user/application/UserFacade.java
+++ api-server/src/main/java/com/kakaopaysec/api/domain/user/application/UserFacade.java
@@ -1,7 +1,16 @@
 package com.kakaopaysec.api.domain.user.application;
 
+import com.kakaopaysec.api.domain.user.domain.UserCommand;
+import com.kakaopaysec.api.domain.user.domain.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserFacade {
+    private final UserService userService;
+
+    public Long registerUser(UserCommand.RegisterUserRequest request) {
+        return userService.registerUser(request);
+    }
 }
